289cb3c27a60d9e8c1c1dfe8bee6d47f0378bb96
pvergoulis <vergoulispaulos@gmail.com>
2025-06-20 19:06:48 +0300

    Feat: DoctorCard component

--- src/components/DoctorCard/DoctorCard.tsx
+++ src/components/DoctorCard/DoctorCard.tsx
@@ -9,14 +9,11 @@ const DoctorCard = ({firstname, lastname , image, specialization, clinic}: docto
                 <div className=" rounded-2xl flex items-center  justify-center p-2 bg-sky-100">
                     <img src={image} alt="" className="w-40 h-40 rounded-full "/>
                 </div>
-                
-                
-                            <div className="space-y-6  ">
-                                <p className="text-center text-2xl text-sky-500  mt-2 mb-2">{firstname} - {lastname}</p>
-                                <p className="text-center text-xl text-gray-500 mb-2">{specialization.name}</p>
-                                <p className="text-center text-xl text-gray-500 ">{clinic.name}</p>
-                            </div>
-                            
+                <div className="space-y-6  ">
+                    <p className="text-center text-2xl text-sky-500  mt-2 mb-2">{firstname} - {lastname}</p>
+                    <p className="text-center text-xl text-gray-500 mb-2">{specialization.name}</p>
+                    <p className="text-center text-xl text-gray-500 ">{clinic.name}</p>
+                </div>       
             </div>
         </>
     )
