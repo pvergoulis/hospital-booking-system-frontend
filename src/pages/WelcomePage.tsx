e11fc2d05313c386faf37a59c0df938f789213c1
pvergoulis <vergoulispaulos@gmail.com>
2025-06-20 15:13:49 +0300

    feat:WelcomePage

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,28 +1,25 @@
-import { jwtDecode } from "jwt-decode";
 import { useState, useEffect } from "react";
-import {type DecodedToken } from "../types/jwtTypes";
+
 
 const WelcomePage = () => {
   const [username, setUsername] = useState<string | null>(null);
-  const [role , setRole] = useState<string | null>(null)
+ 
     
    useEffect(() => {
     const storedUsername = localStorage.getItem("username");
-    const token = localStorage.getItem("token")
     
     if (storedUsername) {
       setUsername(storedUsername);
     }
-
-    const decoded = jwtDecode<DecodedToken>(token!)
-    setRole(decoded.role)
+    document.title="Parvathy Hospital | Welcome Page"
+    
   }, []);
 
   return (
     <>
       <div className="mt-24">
-        <h1>HAHAH </h1>
-        {/* {role?.includes("ADMIN") && <a href="www.google.com" target="blank">Admin</a>} */}
+        <h1>Welcome, {username}</h1>
+        
       </div>
     </>
   );
