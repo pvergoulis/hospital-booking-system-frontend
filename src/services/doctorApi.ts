cf0b0e99042f9654e57b476b0c2bf4d57a47a9a0
pvergoulis <vergoulispaulos@gmail.com>
2025-06-20 19:08:33 +0300

    Feat: doctorApi getFirstEightDoctors

--- src/services/doctorApi.ts
+++ src/services/doctorApi.ts
@@ -1,13 +1,13 @@
 import { type doctorTypeCard } from "../types/doctorTypes"
 import axios from "axios";
 
-
 const API_URL = "http://localhost:3000/api/doctors"
 
 
 export const getFirstSixDoctors = async (): Promise<doctorTypeCard[]> => {
     const token = localStorage.getItem("token");
-  const res = await axios.get<{ status: boolean; data: doctorTypeCard[] }>(
+    
+    const res = await axios.get<{ status: boolean; data: doctorTypeCard[] }>(
     `${API_URL}/sixDoctors`, {
          headers: {
         Authorization: `Bearer ${token}`,
