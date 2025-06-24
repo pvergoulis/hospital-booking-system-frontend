0a4aba999acf3462c9e6688b2aba63f6ef777bd3
pvergoulis <vergoulispaulos@gmail.com>
2025-06-24 20:24:45 +0300

    Feat: useDoctorStepValidation

--- src/hooks/useDoctorStepValidation.ts
+++ src/hooks/useDoctorStepValidation.ts
@@ -5,30 +5,6 @@ import {
 } from "../utils/createDoctorFormWatchUtils";
 import { type doctorType } from "../types/doctorTypes";
 
-// export const useDoctorStepValidation = (watchedValues: doctorType) => {
-//   const isDisabledStepOne = isStepOneDisabled({
-//     firstname: watchedValues.firstname ?? "",
-//     lastname: watchedValues.lastname ?? "",
-//     experience: watchedValues.experience ?? "",
-//   });
-
-//   const isDisabledStepTwo = isStepTwoDisabled({
-//     image: watchedValues.image ?? "",
-//     amka: watchedValues.amka ?? "",
-//     cv: watchedValues.cv ?? "",
-//   });
-
-//   const isDisabledStepThree = isStepThreeDisabled({
-//     specialization: watchedValues.specialization ?? { _id: "", name: "" },
-//     clinic: watchedValues.clinic ?? { _id: "", name: "" },
-//   });
-
-//   return {
-//     isDisabledStepOne,
-//     isDisabledStepTwo,
-//     isDisabledStepThree,
-//   };
-// };
 export const useDoctorStepValidation = (watchedValues: doctorType) => {
   const isDisabledStepOne = isStepOneDisabled({
     firstname: watchedValues.firstname ?? "",
