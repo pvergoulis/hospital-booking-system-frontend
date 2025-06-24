de0609566b91c6787820aea98e9d5809d2ff91b5
pvergoulis <vergoulispaulos@gmail.com>
2025-06-24 15:22:41 +0300

    Feat: AdminHomePage completed

--- src/pages/AdminPages/AdminHomePage.tsx
+++ src/pages/AdminPages/AdminHomePage.tsx
@@ -1,21 +1,7 @@
 import AdminFeatureCard from "../../components/AdminFeatureCard/AdminFeatureCard";
 
 const AdminHomePage = () => {
-  const features = [
-    {
-      title: "Manage Doctors",
-      img: "/doctor-admin.avif",
-    },
-    {
-      title: "Manage Users",
-      img: "/user.jpg",
-    },
-    {
-      title: "Manage Appointments",
-      img: "/book.avif",
-    },
-  ];
-
+ 
   return (
     <>
       <div className="p-10 min-h-[60vh] bg-gray-50 mt-8">
@@ -26,8 +12,8 @@ const AdminHomePage = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <AdminFeatureCard title="Doctors" image="/doctor-admin.avif" link="/doctor-admin" description= "Manage all registered doctors: add new doctors, edit their profiles, or remove them. Monitor their availability and medical specialties."
 />
-        <AdminFeatureCard title="Users" image="/user.jpg" link="/doctor-user" description= "View and manage user accounts: access patient profiles, update user information, and ensure a smooth user experience across the platform."/>
-        <AdminFeatureCard title="Appointments" image="/book.avif" link="/doctor-appointments" description="Access and manage all appointments between doctors and users. View upcoming visits"/>
+        <AdminFeatureCard title="Users" image="/user.jpg" link="/admin-user" description= "View and manage user accounts: access patient profiles, update user information, and ensure a smooth user experience across the platform."/>
+        <AdminFeatureCard title="Appointments" image="/book.avif" link="/admin-appointments" description="Access and manage all appointments between doctors and users. View upcoming visits"/>
         </div>
       </div>
     </>
