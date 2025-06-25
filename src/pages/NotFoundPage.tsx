80164f6cb62f49c99b0b166719da54b9279ea428
pvergoulis <vergoulispaulos@gmail.com>
2025-06-25 19:00:25 +0300

    Feat: NotFoundPage added

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-red-600 text-2xl">404</h1>
+      <p className="text-xl">Page Not Found</p>
+      <p>Looks like you’ve wandered off the map...</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
