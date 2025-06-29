27be2b26dd38587d6d4e1791202fc4e26643b1fd
pvergoulis <vergoulispaulos@gmail.com>
2025-06-29 18:25:31 +0300

    Feat: Login page decoded

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,8 +15,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { usePasswordToggle } from "../hooks/usePasswordToggle";
 import { loginUser } from "../services/authApi";
-import { jwtDecode } from "jwt-decode";
-import { type DecodedToken } from "../types/jwtTypes";
+
 
 const initialValues = {
   username: "",
