d38b0032b7f0f114f5619298f222be6b0df5eaf1
pvergoulis <vergoulispaulos@gmail.com>
2025-06-29 19:21:01 +0300

    Feat: auth util added

--- src/components/RequireAuthComponent/RequireAuth.tsx
+++ src/components/RequireAuthComponent/RequireAuth.tsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router";
+import { getToken } from "../../utils/authTokenUtils";
 
 const RequireAuth = ({ children }: { children: React.ReactNode }) => {
-  const token = localStorage.getItem("token");
+  const token = getToken()
 
   if (!token) {
     return <Navigate to="/login"  replace />;
@@ -12,18 +13,3 @@ const RequireAuth = ({ children }: { children: React.ReactNode }) => {
 
 export default RequireAuth;
 
-
-// import { Navigate } from "react-router";
-// import { useAuth } from "../../context/AuthContext";
-
-// const RequireAuth = ({ children }: { children: React.ReactNode }) => {
-//   const { token } = useAuth();
-
-//   if (!token) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// };
-
-// export default RequireAuth;
